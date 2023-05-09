e56d380c7d18fc76c51407d02ddfffeb44864188
Caio Stigliani Recamon <caio.stig@gmail.com>
2023-05-08 23:43:01 -0300

    Configuração do model

--- src/main/java/com/api/parkingcontrol/models/ParkingSpotModel.java
+++ src/main/java/com/api/parkingcontrol/models/ParkingSpotModel.java
@@ -11,6 +11,7 @@ import java.util.UUID;
 public class ParkingSpotModel implements Serializable {
     private static final long serialVersionUID = 1L;
 
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
